d89e586ff391adf84b0df6e4ff2bda53566093a6
Gobral <umpalump97@gmail.com>
2019-11-21 07:45:48 +0100

    poprawne crap

--- app/src/main/java/com/example/myapplication/NewsFragment.java
+++ app/src/main/java/com/example/myapplication/NewsFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,9 +11,19 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 /**
@@ -20,8 +31,8 @@ import android.view.ViewGroup;
  */
 public class NewsFragment extends Fragment {
 
-
     private RecyclerView recyclerView;
+    private ArrayList<Wpis> naglowki = new ArrayList<>();;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     @Nullable
@@ -32,21 +43,55 @@ public class NewsFragment extends Fragment {
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.news_recycler_view);
         recyclerView.setHasFixedSize(true);
 
-
-        final FragmentActivity c = getActivity();
-        layoutManager = new LinearLayoutManager(c);
+        LinearLayout lwew = rootView.findViewById(R.id.wew_news_layout);
+        //final FragmentActivity c = getActivity();
+        layoutManager = new LinearLayoutManager(lwew.getContext());
         recyclerView.setLayoutManager(layoutManager);
 
-
-        // specify an adapter (see also next example)
-        String myDataset[] = new String[100];
-        for (int i = 0; i < 100; i++){
-            myDataset[i] = "test " + i;
-        }
-        mAdapter = new MyAdapter(myDataset);
+        mAdapter = new MyAdapter(naglowki, getContext());
         recyclerView.setAdapter(mAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Content content = new Content();
+        content.execute();
+    }
+
+
+
+    private class Content extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String url = "http://slaskwroclaw.pl/strona/articles/index/1";
+            try {
+                Document doc = Jsoup.connect(url).get();
+
+                Elements data = doc.select("div.news-box");
+                int size = data.size();
+                Log.d("size", " " + size);
+                for (int i = 0; i < size; i++){
+                    String imgUrl = data.select("div.news-box").select("img").eq(i).attr("src");
+                    String title = data.select("h3.font-color-red").select("a").eq(i).text();
+                    String wpisUrl = data.select("h3.font-color-red").select("a").eq(i).attr("href");
+                    Log.d("zapytanie", "artyk: " + title);
+                    naglowki.add(new Wpis(wpisUrl, imgUrl, title));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/Wpis.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+public class Wpis {
+    public String link;
+    public String miniaturka;
+    public String tresc;
+
+    public Wpis(String link, String min, String tresc){
+        this.link = link;
+        miniaturka = min;
+        this.tresc = tresc;
+    }
+}
