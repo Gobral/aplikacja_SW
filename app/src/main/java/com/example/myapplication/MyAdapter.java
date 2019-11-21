c2652c0863ff7e6f2e612931f600174e7a47e926
Gobral <umpalump97@gmail.com>
2019-11-21 07:24:07 +0100

    poprawne crap

--- app/src/main/java/com/example/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -1,37 +1,51 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
+    public ArrayList<Wpis> mDataset;
+    public String[] tablica;
+    public Context context;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
+        public ImageView imageView;
         public TextView textView;
-        public MyViewHolder(TextView v) {
+        public MyViewHolder(View v) {
             super(v);
-            textView = v;
+            textView = v.findViewById(R.id.wpis_naglowek);
+            imageView = v.findViewById(R.id.wpis_image_view);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public MyAdapter(ArrayList<Wpis> parametry, Context context) {
+        mDataset = parametry;
+        this.context = context;
     }
+
 
     // Create new views (invoked by the layout manager)
     @Override
     public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.news_sr_view, parent, false);
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
@@ -42,13 +56,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
+        holder.textView.setText(mDataset.get(position).tresc);
+        Picasso.get().load(mDataset.get(position).miniaturka).into(holder.imageView);
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset.size();
     }
 }
