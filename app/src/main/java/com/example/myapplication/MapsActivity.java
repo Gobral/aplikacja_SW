9ace21d4df277dce7c0573b03b42e5f10a99304b
Gobral <umpalump97@gmail.com>
2020-01-26 03:42:26 +0100

    wysuwa menu na mapach, domyslnie startuje newsy, trzeba dodac bazee parkingow

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,19 +1,45 @@
 package com.example.myapplication;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationRequest mLocationRequest;
+    private Location location;
+    private Marker user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +49,119 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ImageButton imageButton = findViewById(R.id.parkingi_menu_button);
+        RelativeLayout menu = findViewById(R.id.parkingi_menu);
+        menu.setVisibility(menu.GONE);
+
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); // add this
+        fadeIn.setDuration(600);
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                menu.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
+        fadeOut.setStartOffset(100);
+        fadeOut.setDuration(600);
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                menu.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(menu.getVisibility() == menu.VISIBLE){
+                    menu.startAnimation(fadeOut);
+                    //menu.setVisibility(menu.GONE);
+                }
+                else {
+                    menu.startAnimation(fadeIn);
+                    //menu.setVisibility(menu.VISIBLE);
+                }
+            }
+        });
+
+        GoogleApiAvailability availability
+                = GoogleApiAvailability.getInstance();
+        int result = availability.isGooglePlayServicesAvailable(this);
+        if (result != ConnectionResult.SUCCESS) {
+            if (!availability.isUserResolvableError(result)) {
+                Toast.makeText(this, "Przyznaj uprawnienia GPS!", Toast.LENGTH_LONG).show();
+            }
+        }
+        location = new Location("domyslne");
+        location.setLongitude(17.022222);
+        location.setLatitude(51.11);
+
+        LocationCallback mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                location = new Location(locationResult.getLastLocation());
+                if (location != null) {
+                    if (mMap != null) {
+                        LatLng latLng = new LatLng(location.getLatitude(),
+                                location.getLongitude());
+                        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                        user.setPosition(latLng);
+
+                    }
+                }
+            }
+        };
+
+        FusedLocationProviderClient fusedLocationClient = null;
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        LocationRequest request = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(500); // Update every 5 seconds.
+        fusedLocationClient.requestLocationUpdates(request, mLocationCallback, null);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(51.11, 17.022222);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setMinZoomPreference(5.0f);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+        user = mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Tu jesteś").icon(BitmapDescriptorFactory.fromResource(R.drawable.user25)));
     }
 }
